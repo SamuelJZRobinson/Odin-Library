06124af0999ace1ee2f20f530a946ac0e721d8e3
Samuel Robinson <samueljzrobinson@gmail.com>
2025-02-12 18:14:52 +1000

    Document Setup And Navbar

---
+++ js/core.js
@@ -0,0 +1,34 @@
+const BUT_ADD_BOOK = document.querySelector("#add-book");
+
+function Library(){
+  this.books = [];
+
+  this.addBook = function(){
+    // Get data from form
+    const newBook = new Book("title", "author", "genre", "pages", "favourite", "status");
+    this.books.push(newBook);
+  }
+
+  this.getBooks = function(){
+    return this.books;
+  }
+}
+
+function Book(title, author, genre, pages, favourite, status){
+ this.title = title;
+ this.author = author;
+ this.genre = genre;
+ this.totalPages = pages;
+ this.favourite = favourite;
+ this.status = status;
+
+ this.getInfo = function(){
+  return [title,author,genre,totalPages,favourite,status];
+ }
+}
+
+// Create Library
+const library = new Library();
+
+library.addBook();
+console.log(library.getBooks());
