ff675ed7ade61df7750bf4e9b32bbcff93cb8b28
Samuel Robinson <samueljzrobinson@gmail.com>
2025-02-12 22:17:00 +1000

    Added Classes And Fixed Library Add Book

--- js/core.js
+++ js/core.js
@@ -1,50 +1,69 @@
-const FORM_CONTAINER = document.querySelector("#form-container");
-const FORM_ADD_BOOK = document.querySelector("#form-add-book");
-const BUT_ADD_BOOK = document.querySelector("#add-book");
-const BUT_CLOSE_FORM = document.querySelector("#form-close");
-
-BUT_ADD_BOOK.addEventListener("click",(e) =>{
-  FORM_CONTAINER.style.display = "block";
-})
-
-BUT_CLOSE_FORM.addEventListener("click",(e) =>{
-  FORM_CONTAINER.style.display = "none";
-  FORM_ADD_BOOK.reset();
-});
-
-function closeForm(){
-
+class Library{
+  constructor(){
+    this.books = [];
+  }
+    
+    addBook(title, author, genre, pages, favourite, readStatus){
+      const newBook = new Book(title, author, genre, pages, favourite, readStatus);
+      this.books.push(newBook);
+    }
+    
+    getBooks(){
+      return this.books;
+    }
 }
 
-function Library(){
-  this.books = [];
-
-  this.addBook = function(){
-    // Get data from form
-    const newBook = new Book("title", "author", "genre", "pages", "favourite", "status");
-    this.books.push(newBook);
+class Book{
+  constructor(title, author, genre, pages, favourite, readStatus) {
+    this.title = title;
+    this.author = author;
+    this.genre = genre;
+    this.totalPages = pages;
+    this.favourite = favourite;
+    this.readStatus = readStatus;
   }
 
-  this.getBooks = function(){
-    return this.books;
-  }
-}
-
-function Book(title, author, genre, pages, favourite, status){
- this.title = title;
- this.author = author;
- this.genre = genre;
- this.totalPages = pages;
- this.favourite = favourite;
- this.Readstatus = status;
-
- this.getInfo = function(){
-  return [title,author,genre,totalPages,favourite,status];
+ getAllInfo(){
+  return [this,title, this.author,this.genre, this.totalPages, this.favourite, this.readStatus];
  }
 }
 
 // Create Library
 const library = new Library();
 
-library.addBook();
-console.log(library.getBooks());
+// DOM Elements
+const FORM_CONTAINER = document.querySelector("#form-container");
+const FORM_ADD_BOOK = document.querySelector("#form-add-book");
+const BUT_OPEN_FORM = document.querySelector("#but-open");
+const BUT_CLOSE_FORM = document.querySelector("#but-close");
+
+// Open Form
+BUT_OPEN_FORM.addEventListener("click",(e) =>{
+  FORM_CONTAINER.style.display = "block";
+})
+
+// Close Form
+BUT_CLOSE_FORM.addEventListener("click",(e) =>{
+  closeForm();
+})
+
+function closeForm() {
+  FORM_CONTAINER.style.display = "none";
+  FORM_ADD_BOOK.reset();
+}
+
+// Submit Form
+FORM_ADD_BOOK.addEventListener("submit",(e)=>{
+  const title = document.querySelector("#title").value.trim();
+  const author = document.querySelector("#author").value.trim();
+  const genre = document.querySelector("#genre").value;
+  const totalPages = parseInt(document.querySelector("#total-pages").value);
+  const readStatus = document.querySelector("#read-status").checked;
+  const favourite = document.querySelector("#favourite").checked;
+
+  library.addBook(title, author, genre, totalPages, readStatus, favourite);
+
+  closeForm();
+  console.log(library.getBooks());
+  e.preventDefault();
+})
