ce1e5eba242ae5e9d7f4b44877a7bafa3de72f1e
Samuel Robinson <samueljzrobinson@gmail.com>
2025-02-19 19:21:21 +1000

    Added Duplicate Book Title Check

--- js/core.js
+++ js/core.js
@@ -21,15 +21,23 @@ BUT_CLOSE_FORM.addEventListener("click",(e) =>{
 
 // Submit Form
 FORM_ADD_BOOK.addEventListener("submit",(e) =>{
+  // Stop default form submission
   e.preventDefault();
-  const title = document.querySelector("#title").value.trim();
-  const author = document.querySelector("#author").value.trim();
-  const genre = document.querySelector("#genre").value;
-  const totalPages = parseInt(document.querySelector("#total-pages").value);
-  const readStatus = document.querySelector("#read-status").checked;
-  const NEW_BOOK = new Book(title, author, genre, totalPages, readStatus);
-  library.addBook(NEW_BOOK);
-  closeForm();
+  const TITLE = document.querySelector("#title").value.trim();
+  const AUTHOR = document.querySelector("#author").value.trim();
+  const GENRE = document.querySelector("#genre").value;
+  const TOTAL_PAGES = parseInt(document.querySelector("#total-pages").value);
+  const READ_STATUS = document.querySelector("#read-status").checked;
+  const NEW_BOOK = new Book(TITLE, AUTHOR, GENRE, TOTAL_PAGES, READ_STATUS);
+
+  const BOOK_EXISTS = library.books.some(book => book.title === TITLE);
+  if (BOOK_EXISTS) {
+    alert("This book already exists");
+  }
+  else {
+    library.addBook(NEW_BOOK);
+    closeForm();
+  }
 })
 
 // Core Logic
